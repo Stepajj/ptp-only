05058270f755573439bce102dfec6f9fda291b9d
Stepajj <stepagrablevskij@gmail.com>
2026-08-19 01:51:29 +0200

    pixel perfect 0.1

--- apps/frontend/src/app/(protected)/dashboard/ui/Dashboard.tsx
+++ apps/frontend/src/app/(protected)/dashboard/ui/Dashboard.tsx
@@ -1,5 +1,6 @@
 import styles from './Dashboard.module.css';
 import ActiveOrdersCard from './active-orders-card/ActiveOrdersCard';
+
 import TodayIncomeCard from './TodayIncomeCard/TodayIncomeCard';
 import BalanceCard from './balance-card/BalanceCard';
 import QuickActions from './quick-actions/QuickActions';
@@ -8,6 +9,15 @@ import RecentOrders from './recent-orders/RecentOrders';
 export default function Dashboard() {
   return (
     <section className={styles.dashboard}>
+      <h1 className={styles.title}>
+  Здравствуйте, пользователь 👋
+</h1>
+
+<p className={styles.subtitle}>
+  <span className={styles.subtitleBold}>2 реквизита</span>
+  <span className={styles.dot} />
+  <span>система подбирает переводы</span>
+</p>
       <div className={styles.statsGrid}>
     <BalanceCard />
 
